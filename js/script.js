5b0765833b2167487eca58b004e8630db0a321b2
pablopena1223 <lito23.pjp@gmail.com>
2019-03-19 22:19:28 +0000

    first day

--- js/script.js
+++ js/script.js
@@ -3,9 +3,20 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+var giphy_api_url = 'https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC'
+
 $("#search-button").click(function(){
-  
-  
-  
+
+  $.ajax({
+        url: giphy_api_url,
+        method: "GET",
+        success: function(response) {
+            var originalImage = response.data[0].images.fixed_width.url;
+            $('body').append('<img src="' +  originalImage + '"/>');
+            // YOUR CODE GOES HERE
+        }
+    });
+
+
 });
 
